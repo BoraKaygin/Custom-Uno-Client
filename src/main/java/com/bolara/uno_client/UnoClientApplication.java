afe095caeb19064080b3253327436f420a9874cf
Bora Kaygin <e2580702@ceng.metu.edu.tr>
2025-04-20 17:16:15 +0300

    Added project files

---
+++ src/main/java/com/bolara/uno_client/UnoClientApplication.java
@@ -0,0 +1,13 @@
+package com.bolara.uno_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnoClientApplication.class, args);
+	}
+
+}
